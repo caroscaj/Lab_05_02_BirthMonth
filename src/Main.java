6e382048d9f92f8ca5f500e6f03cf4be38761e39
caroscaj <caroscaj@mail.uc.edu>
2022-10-03 17:08:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int birthMonth =13;
+
+        if (birthMonth >= 1 & birthMonth <= 12)
+        {
+            System.out.println("Your birth month is " + birthMonth);
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month");
+        }
+    }
+}
